5708274e633b400c62c68b423db9b0c11356bce3
Moamin <moamin.salamah@gmail.com>
2022-04-08 10:19:05 +0200

    1st commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      primary: '#ffe599'
+    },
+    fontFamily:{
+      sans: ['Montserrat', 'sans-serif']
+    }
+  },
+  plugins: [],
+}
